f0401bc2953002890c54ef349c91fd4d5b098849
daniellacosse <d@niellacos.se>
2021-02-04 16:27:39 -0600

    actor detail page!

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../elements/Header/Header";
+
+import ActorDetails from "../ActorDetails/ActorDetails";
 import Home from "../Home/Home";
 import Movie from "../Movie/Movie";
 import NotFound from "../elements/NotFound/NotFound";
@@ -16,6 +18,11 @@ const App = () => {
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/:movieId" component={Movie} exact />
+          <Route
+            path="/actor-details/:actorId"
+            component={ActorDetails}
+            exact
+          />
           <Route component={NotFound} />
         </Switch>
       </>
--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { IMAGE_BASE_URL } from '../../../config';
-import './Actor.css';
+import React from "react";
+import { IMAGE_BASE_URL } from "../../../config";
+import "./Actor.css";
 
-const Actor = props => {
-  const POSTER_SIZE = 'w154';
-  // console.log(props.actor);
+const Actor = (props) => {
+  const POSTER_SIZE = "w154";
+  // console.log(props);
   return (
-    <div className='rmdb-actor'>
-      <img
-        src={
-          props.actor.profile_path
-            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}`
-            : './images/no_image.jpg'
-        }
-        alt='actorthumb'
-      />
-      <span className='rmdb-actor-name'>{props.actor.name}</span>
-      <span className='rmdb-actor-character'>{props.actor.character}</span>
-    </div>
+    <a href={`/movie-finder/actor-details/${props.actor.id}`}>
+      <div className="rmdb-actor">
+        <img
+          src={
+            props.actor.profile_path
+              ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}`
+              : "./images/no_image.jpg"
+          }
+          alt="actorthumb"
+        />
+        <span className="rmdb-actor-name">{props.actor.name}</span>
+        <span className="rmdb-actor-character">{props.actor.character}</span>
+      </div>
+    </a>
   );
 };
 
